a01d7f7a81c697246a059e2f521570a9a73c4647
meowsl <VlaFrank@ya.ru>
2023-10-09 16:39:26 +0300

    some adaptive banner

--- client/components/AppBanner.vue
+++ client/components/AppBanner.vue
@@ -1,33 +1,49 @@
 <template>
-  <div class="banner d-flex align-end pt-16">
-    <VContainer class="banner-block pb-3">
-      <VRow class="text-white">
-        <VCol class="d-flex align-center ps-0 ps-xxl-16">
-          <div class="test-content d-flex flex-column">
-            <p class="text-h2 font-weight-bold">ROBO SCHOOL</p>
-            <p class="my-12 text-white text-h6">Курсы повышения квалификации по робототехнике для
+  <div class="banner d-flex align-end pt-16 mt-0 mt-md-10 mt-lg-2">
+    <VContainer class="banner-block pb-3 px-0">
+      <VRow class="text-white d-flex flex-column flex-lg-row">
+        <VCol class="d-flex align-center justify-center px-12 px-sm-16 px-lg-0">
+          <div class="test-content d-flex flex-column text-center text-lg-start">
+            <p class="text-h4 text-md-h3 text-lg-h2 font-weight-bold">ROBO SCHOOL</p>
+            <p class="my-5 my-lg-12 text-white text-subtitle-1 text-md-h6">Курсы повышения квалификации по робототехнике
+              для
               педагогов начальной школы</p>
             <VBtn
               variant="tonal"
               size="large"
-              class="banner-block__btn text-white "
-              height="80"
-              width="260"
+              class="banner-block__btn d-none d-lg-flex text-white mx-auto mx-lg-0"
+              :height="80"
+              :width="auto"
+              max-height="80"
+              max-width="260"
             >
               <p class="banner-block__btn__text text-white text-subtitle-1">Записаться
                 на курс</p>
             </VBtn>
           </div>
         </VCol>
-        <VCol class="pb-0 d-flex justify-center">
+        <VCol class="pb-6 pb-lg-0 d-flex justify-center px-10 px-lg-0 flex-column">
           <VImg
-            class="banner-block__img align-end"
+            class="banner-block__img align-end mx-16 mx-lg-0"
             :src="bannerImage"
-            width="auto"
-            height="auto"
-            max-width="auto"
+            :width="auto"
+            :height="auto"
+            max-width="700"
             max-height="800"
           />
+          <VBtn
+            variant="tonal"
+            block
+            size="large"
+            class="banner-block__btn d-flex d-lg-none text-white mx-auto mx-lg-0"
+            :height="80"
+            :width="auto"
+            max-height="80"
+            max-width="260"
+          >
+            <p class="banner-block__btn__text  text-white text-subtitle-1">Записаться
+              на курс</p>
+          </VBtn>
         </VCol>
       </VRow>
     </VContainer>
--- client/components/AppDescription.vue
+++ client/components/AppDescription.vue
@@ -8,28 +8,30 @@
         <a class="font-weight-bold">Robo School</a> – учреждение для формирования кадрового педагогического резерва в
         сфере робототехники и программирования
       </p>
-      <v-row class="block-description__text mt-10">
-        <v-col>
-          <p class="num">10</p>
+      <VRow class="block-description__text text-h6 font-weight-regular mt-10">
+        <VCol>
+          <p class="num font-weight-bold pb-10">10</p>
           УМК по различным направлениям по робототехнике
-        </v-col>
-        <v-col>
-          <p class="num">20</p>
+        </VCol>
+        <VCol>
+          <p class="num font-weight-bold pb-10">20</p>
           Школ, в которых запущена робототехника
-        </v-col>
-        <v-col>
-          <p class="num">100</p>
+        </VCol>
+        <VCol>
+          <p class="num font-weight-bold pb-10">100</p>
           Педагогов прошедших курсы повышения квалификации
-        </v-col>
-        <v-col>
-          <p class="num">10 000</p>
+        </VCol>
+        <VCol>
+          <p class="num font-weight-bold pb-10">10 000</p>
           Обученных детей на базе собственных центров
-        </v-col>
-      </v-row>
+        </VCol>
+      </VRow>
     </VContainer>
   </div>
 </template>
 
 
 <script lang="ts">
+import { VRow } from 'vuetify/lib/components'
+
 </script>
